5a0741bd9bbd57de98b6f7b94b575615ffc960cd
a-yuto <arakiyutoam@gmail.com>
2019-11-30 02:34:41 +0000

    patternmatch

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,1 @@
-fn main(){for i in 0..101{if(i%15==0){println!("fizzbuzz");}else if(i%3==0){println!("fizz");}else if(i%5==0){println!("buzz");}else{println!("{}",i);}}}
+fn main()for i in 0..101{match i%15{0=>print!("fizzbuzz\n"),3|6|9|12=>print!("fizz\n"),5|10=>print!("buzz\n"),_=>print!("{}\n",i),}}}
