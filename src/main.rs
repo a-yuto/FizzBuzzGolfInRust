433ebf0b6185f69c6347788ac436bfc9795c4cfe
a-yuto <arakiyutoam@gmail.com>
2019-11-30 08:32:57 +0000

    12も加えないといけません

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,1 @@
-fn main(){for i in 0..101{println!("{}",match i%15{0=>"fizzbuzz",3|6|9=>"fizz",5|10=>"buzz",_=>{print!("{}",i);""}});}}
+fn main(){for i in 1..101{println!("{}",match i%15{0=>"fizzbuzz",3|6|9|12=>"fizz",5|10=>"buzz",_=>{print!("{}",i);""}});}}
