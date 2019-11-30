eda8648b043e5b9c724dc39ca2e79a5292ca0140
a-yuto <arakiyutoam@gmail.com>
2019-11-30 07:50:12 +0000

    println！のなかにmatchを書いてその中にprintを書いた。

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,1 @@
-fn main()for i in 0..101{match i%15{0=>print!("fizzbuzz\n"),3|6|9|12=>print!("fizz\n"),5|10=>print!("buzz\n"),_=>print!("{}\n",i),}}}
+fn main(){for i in 0..101{println!("{}",match i%15{0=>"fizzbuzz",3|6|9=>"fizz",5|10=>"buzz",_=>{print!("{}",i);""}});}}
